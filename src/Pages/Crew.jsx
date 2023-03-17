cd35474cbcf6e96f99bc38aa5649ba9f9c91ed84
James <james.mcgrory135@gmail.com>
2023-03-17 18:10:14 +1100

    Crew page now utlises data, crew member information accessed by button

--- src/Pages/Crew.jsx
+++ src/Pages/Crew.jsx
@@ -5,13 +5,14 @@ import {data} from '/data.js'
 
 export default function Crew() {
 
-    // 0 == moon, 1 == mars, 2 == europa, 3 == Titan
-    const [destination, setDestination] = useState(data.destinations[0])
+    // 0 == Douglas, 1 == Mark, 2 == Victor, 3 == Anousheh
+    const [crew, setCrew] = useState(data.crew[0])
 
-    function handleDestination(index) {
-        setDestination(data.destinations[index])
+    function handleCrew(index) {
+        setCrew(data.crew[index])
     }
 
+    console.log(crew)
     return (
         <div className='bg-fixed bg-mobile-crew md:bg-tablet-crew xl:bg-desktop-crew min-h-screen w-screen bg-no-repeat bg-cover flex flex-col pb-8'>
             <Nav />
@@ -23,28 +24,35 @@ export default function Crew() {
                         <p className="text-primary font-barlow-condensed uppercase tracking-[0.16875rem] xl:tracking-[0.295rem]">Meet your crew</p>
                     </div>
                     <img 
-                        className="max-w-[11.0625rem]"
-                        src="/crew/image-douglas-hurley.png" 
+                        className="max-h-[13.875rem]"
+                        src={crew.images.png}
                         alt="crew member photo" />
                 </div>
 
                 <div className="flex flex-col items-center">
                     <div className="flex gap-x-4 mb-8">
-                        <button className="bg-gray-600 h-3 w-3 rounded-full hover:bg-gray-400"></button>
-                        <button className="bg-gray-600 h-3 w-3 rounded-full hover:bg-gray-400"></button>
-                        <button className="bg-gray-600 h-3 w-3 rounded-full hover:bg-gray-400"></button>
-                        <button className="bg-gray-600 h-3 w-3 rounded-full hover:bg-gray-400"></button>
+                        <button
+                            onClick={() => handleCrew(0)} 
+                            className="bg-gray-600 h-3 w-3 rounded-full hover:bg-gray-400"></button>
+                        <button
+                            onClick={() => handleCrew(1)}  
+                            className="bg-gray-600 h-3 w-3 rounded-full hover:bg-gray-400"></button>
+                        <button
+                            onClick={() => handleCrew(2)}  
+                            className="bg-gray-600 h-3 w-3 rounded-full hover:bg-gray-400"></button>
+                        <button
+                            onClick={() => handleCrew(3)}  
+                            className="bg-gray-600 h-3 w-3 rounded-full hover:bg-gray-400"></button>
                     </div>
                     <div>
                         <p className="uppercase text-center font-bellefair text-primary/50 mb-2">
-                            Commander
+                            {crew.role}
                         </p>
                         <h1 className="uppercase text-center font-bellefair text-primary text-2xl mb-4">
-                            Douglas Hurley
+                            {crew.name}
                         </h1>
                         <p className="font-barlow text-secondary leading-6 text-center">
-                            Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. 
-                            He launched into space for the third time as commander of Crew Dragon Demo-2.
+                            {crew.bio}
                         </p>
                     </div>
                 </div>
