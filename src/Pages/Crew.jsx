3631dbd0592a72c835aadd2e4ae385339d49f014
James <james.mcgrory135@gmail.com>
2023-03-20 11:13:11 +1100

    made crew page tablet responsive

--- src/Pages/Crew.jsx
+++ src/Pages/Crew.jsx
@@ -18,18 +18,18 @@ export default function Crew() {
             <Nav />
             <div className="mx-6 mt-4">
 
-                <div className="border-b border-gray-700 mb-8 flex flex-col items-center gap-y-8">
+                <div className="border-b border-gray-700 mb-8 flex flex-col items-center gap-y-8 md:border-none md:mb-14">
                     <div className="flex gap-x-4 md:self-start md:ml-10 md:text-[1.25rem] xl:-ml-10 xl:text-[1.75rem]">
                         <p className="text-gray-500 font-barlow-condensed uppercase tracking-[0.16875rem] font-bold xl:tracking-[0.295rem]">02</p>
                         <p className="text-primary font-barlow-condensed uppercase tracking-[0.16875rem] xl:tracking-[0.295rem]">Meet your crew</p>
                     </div>
                     <img 
-                        className="max-h-[13.875rem]"
+                        className="max-h-[13.875rem] md:hidden"
                         src={crew.images.png}
                         alt="crew member photo" />
                 </div>
 
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center md:flex-col-reverse">
                     <div className="flex gap-x-4 mb-8">
                         <button
                             onClick={() => handleCrew(0)} 
@@ -45,18 +45,21 @@ export default function Crew() {
                             className="bg-gray-600 h-3 w-3 rounded-full hover:bg-gray-400"></button>
                     </div>
                     <div>
-                        <p className="uppercase text-center font-bellefair text-primary/50 mb-2">
+                        <p className="uppercase text-center font-bellefair text-primary/50 mb-2 md:text-2xl">
                             {crew.role}
                         </p>
-                        <h1 className="uppercase text-center font-bellefair text-primary text-2xl mb-4">
+                        <h1 className="uppercase text-center font-bellefair text-primary text-2xl mb-4 md:text-[2.5rem]">
                             {crew.name}
                         </h1>
-                        <p className="font-barlow text-secondary leading-6 text-center">
+                        <p className="font-barlow text-secondary leading-6 text-center md:max-w-[30.25rem] md:mb-10 min-h-[6rem]">
                             {crew.bio}
                         </p>
                     </div>
                 </div>
-                
+                <img 
+                    className="hidden md:block max-h-[35.75rem] mx-auto border-b"
+                    src={crew.images.png}
+                    alt="crew member photo" />
             </div>
         </div>
     )
